7682dcb78f2410fd8db0d7e01c297be96661ef42
Antonio Fabregat <fabregat.antonio@gmail.com>
2016-01-16 11:08:51 +0000

    configuration update

--- src/main/java/org/reactome/server/tools/config/RESTConfiguration.java
+++ src/main/java/org/reactome/server/tools/config/RESTConfiguration.java
@@ -1,4 +1,4 @@
-package org.reactome.server.tools.controller;
+package org.reactome.server.tools.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
