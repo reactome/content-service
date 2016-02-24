fe57ef79becd76c2450ef84814d6975b1d3fcb05
gviteri <gviteri@ebi.ac.uk>
2016-02-24 14:51:31 +0000

    Download

--- src/main/java/org/reactome/server/tools/controller/PsicquicInteractionsController.java
+++ src/main/java/org/reactome/server/tools/controller/PsicquicInteractionsController.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.reactome.server.tools.interactors.model.PsicquicResource;
+import org.reactome.server.tools.manager.DownloadManager;
 import org.reactome.server.tools.manager.InteractionManager;
 import org.reactome.server.tools.model.interactors.Interactors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,9 @@ public class PsicquicInteractionsController {
 
     @Autowired
     private InteractionManager interactions;
+
+    @Autowired
+    public DownloadManager downloadManager;
 
     @ApiOperation(value = "Retrieve a list of all Psicquic Registries services", response = PsicquicResource.class, produces = "application/json")
     @RequestMapping(value = "/resources", method = RequestMethod.GET, produces = "application/json")
@@ -52,7 +56,7 @@ public class PsicquicInteractionsController {
     }
 
     @ApiOperation(value = "Retrieve a summary of a given accession by resource", response = Interactors.class, produces = "application/json")
-    @RequestMapping(value = "/{resource}/{acc}/summary", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/molecule/{resource}/{acc}/summary", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public Interactors getProteinSummaryByResource(@ApiParam(value="PSICQUIC Resource",required = true) @PathVariable String resource,
                                                    @ApiParam(value="Single Accession",required = true) @PathVariable String acc)  {
@@ -72,5 +76,36 @@ public class PsicquicInteractionsController {
         return interactions.getPsicquicProteinsSummary(accs, resource);
 
     }
-
+//
+//    @ApiOperation(value = "Downloads PSICQUIC interactions for a given accession",
+//            notes = "The results are filtered by the selected resource. The filename is the one to be suggested in the download window.")
+//    @RequestMapping(value = "/molecule/{resource}/{acc}/download/{filename}.csv", method = RequestMethod.GET, produces = "text/csv")
+//    @ResponseBody
+//    public FileSystemResource downloadPsicquicMoleculeInteractions(@ApiParam(name = "acc", value = "the accession", required = true)
+//                                                                   @PathVariable String acc,
+//                                                                   @ApiParam(name = "resource", value = "the psicquic resource", required = true)
+//                                                                   @PathVariable String resource,
+//                                                                   @ApiParam(name = "filename", value = "the file name for the downloaded information", required = true, defaultValue = "result")
+//                                                                   @PathVariable String filename) throws IOException {
+//
+//        return downloadManager.downloadPsicquicMoleculeInteractions(acc, resource, filename);
+//    }
+//
+//    //@ApiIgnore // Shall we change the url just for security ?
+//    @ApiOperation(value = "Downloads PSICQUIC interactions for a given accessions",
+//            notes = "The results are filtered by the selected resource. The filename is the one to be suggested in the download window.")
+//    @RequestMapping(value = "/molecules/{resource}/download/{filename}.csv", method = RequestMethod.POST, produces = "text/csv", headers = "Content-Type=application/x-www-form-urlencoded")
+//    @ResponseBody
+//    public FileSystemResource downloadPsicquicMoleculesInteractions(@ApiParam(value = "Interactor accessions (or identifiers)", required = true)
+//                                                                    @RequestParam String molecules,
+//                                                                    @ApiParam(name = "resource", value = "the psicquic resource", required = true)
+//                                                                    @PathVariable String resource,
+//                                                                    @ApiParam(name = "filename", value = "the file name for the downloaded information", required = true, defaultValue = "result")
+//                                                                    @PathVariable String filename) throws IOException {
+//
+//        /** Split param and put into a Set to avoid duplicates **/
+//        Set<String> accs = new HashSet<>(Arrays.asList(molecules.split("\\s*,\\s*")));
+//
+//        return downloadManager.downloadPsicquicMoleculesInteractions(accs, resource, filename);
+//    }
 }
--- src/main/java/org/reactome/server/tools/controller/StaticInteractionsController.java
+++ src/main/java/org/reactome/server/tools/controller/StaticInteractionsController.java
@@ -3,6 +3,7 @@ package org.reactome.server.tools.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.reactome.server.tools.manager.DownloadManager;
 import org.reactome.server.tools.manager.InteractionManager;
 import org.reactome.server.tools.model.interactors.Interactors;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,9 @@ public class StaticInteractionsController {
 
     @Autowired
     private InteractionManager interactions;
+
+    @Autowired
+    public DownloadManager downloadManager;
 
     @ApiOperation(value = "Retrieve a summary of a given accession", response = Interactors.class, produces = "application/json")
     @RequestMapping(value = "/molecule/{acc}/summary", method = RequestMethod.GET, produces = "application/json")
@@ -63,4 +67,33 @@ public class StaticInteractionsController {
         Set<String> accs = new HashSet<>(Arrays.asList(proteins.split("\\s*,\\s*")));
         return interactions.getStaticProteinDetails(accs, STATIC_RESOURCE_NAME, page, pageSize);
     }
+
+//    @ApiOperation(value = "Downloads interactions for a given accession",
+//            notes = "Download interactions from IntAct (static). The filename is the one to be suggested in the download window.")
+//    @RequestMapping(value = "/molecule/download/{acc}/{filename}.csv", method = RequestMethod.GET, produces = "text/csv")
+//    @ResponseBody
+//    public FileSystemResource downloadStaticInteraction(@ApiParam(name = "acc", value = "the accession", required = true)
+//                                                        @PathVariable String acc,
+//                                                        @ApiParam(name = "filename", value = "the file name for the downloaded information", required = true, defaultValue = "result")
+//                                                        @PathVariable String filename) throws IOException {
+//
+//
+//        return downloadManager.downloadStaticMoleculeInteractions(acc, filename);
+//    }
+//
+//    //@ApiIgnore // Shall we change the url just for security ?
+//    @ApiOperation(value = "Downloads interactions for a given accessions",
+//            notes = "Download interactions from IntAct (static). The filename is the one to be suggested in the download window.")
+//    @RequestMapping(value = "/molecules/download/{filename}.csv", method = RequestMethod.POST, produces = "text/csv", headers = "Content-Type=application/x-www-form-urlencoded")
+//    @ResponseBody
+//    public FileSystemResource downloadStaticInteractions(@ApiParam(value = "Interactor accessions (or identifiers)", required = true)
+//                                                         @RequestParam(required = true) String molecules,
+//                                                         @ApiParam(name = "filename", value = "the file name for the downloaded information", required = true, defaultValue = "result")
+//                                                         @PathVariable String filename) throws IOException {
+//
+//        /** Split param and put into a Set to avoid duplicates **/
+//        Set<String> accs = new HashSet<>(Arrays.asList(molecules.split("\\s*,\\s*")));
+//
+//        return downloadManager.downloadStaticMoleculesInteractions(accs, filename);
+//    }
 }
