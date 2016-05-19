3729d4d75e4e902dcd693c83d0c743a37d06a614
flo <fkorninger@yahoo.de>
2016-05-19 11:18:33 +0100

    Replicating methods for the graph controller

--- src/main/java/org/reactome/server/service/controller/graph/GraphController.java
+++ src/main/java/org/reactome/server/service/controller/graph/GraphController.java
@@ -3,12 +3,14 @@ package org.reactome.server.service.controller.graph;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang.StringUtils;
 import org.reactome.server.graph.domain.model.*;
 import org.reactome.server.graph.domain.result.Participant;
 import org.reactome.server.graph.domain.result.SimpleDatabaseObject;
 import org.reactome.server.graph.service.DatabaseObjectService;
 import org.reactome.server.graph.service.GeneralService;
 import org.reactome.server.graph.service.ParticipantService;
+import org.reactome.server.graph.service.helper.RelationshipDirection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
@@ -28,88 +30,95 @@ import java.util.Collection;
 public class GraphController {
 
     @Autowired
-    private DatabaseObjectService databaseObjectService;
-    @Autowired
     private GeneralService generalService;
-    @Autowired
-    private ParticipantService participantService;
-//    @Autowired
-//    private EventService eventService;
-//    @Autowired
-//    private PhysicalEntityService physicalEntityService;
 
-//    private FileWriter writer;
-
-    public GraphController() throws IOException {
-//        writer = new FileWriter(new File("test.txt"));
-    }
-
-    @ApiOperation(value = "Retrieves all Reactome top level pathways",response = DatabaseObject.class, responseContainer = "List", produces = "application/json")
-    @RequestMapping(value = "/topLevelPathway", method = RequestMethod.GET, produces = "application/json")
+    @ApiOperation(value = "Retrieves version number of current database",response = Integer.class, produces = "application/json")
+    @RequestMapping(value = "/version", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public Collection<TopLevelPathway> getTopLevelPathways()  {
-        return generalService.getTopLevelPathways();
+    public Integer getDBVersion()  {
+        return generalService.getDBVersion();
     }
 
-    @ApiOperation(value = "Retrieves all Reactome top level pathways for given species name",response = DatabaseObject.class, responseContainer = "List", produces = "application/json")
-    @RequestMapping(value = "/topLevelPathway/{speciesName}", method = RequestMethod.GET, produces = "application/json")
+    @ApiOperation(value = "Retrieves name of current database",response = String.class, produces = "application/json")
+    @RequestMapping(value = "/getDBName", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public Collection<TopLevelPathway> getTopLevelPathways(@ApiParam(defaultValue = "Homo sapiens",required = true) @PathVariable String speciesName)  {
-        return generalService.getTopLevelPathways(speciesName);
+    public String getDBName()  {
+        return generalService.getDBName();
     }
 
+    //todo speciesList ??? right name here ?, rename
     @ApiOperation(value = "Retrieves all species",response = Species.class, responseContainer = "List", produces = "application/json")
-    @RequestMapping(value = "/species", method = RequestMethod.GET, produces = "application/json")
+    @RequestMapping(value = "/speciesList", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public Collection<Species> getSpecies()  {
+    public Collection<Species> getAllSpecies()  {
         return generalService.getAllSpecies();
     }
 
-    @ApiOperation(value = "Retrieves details of a databaseObject",response = DatabaseObject.class, produces = "application/json")
-    @RequestMapping(value = "/detail/{id}", method = RequestMethod.GET, produces = "application/json")
+    @ApiOperation(value = "Retrieves all diseases",response = String.class, produces = "text/plain")
+    @RequestMapping(value = "/getDiseases", method = RequestMethod.GET, produces = "text/plain")
     @ResponseBody
-    public DatabaseObject queryById(@ApiParam(defaultValue = "R-HSA-1640170",required = true) @PathVariable String id) throws IOException {
-//        Long start, time;
-//        start = System.currentTimeMillis();
-
-        DatabaseObject databaseObject = databaseObjectService.findById(id);
-//        DatabaseObject databaseObject  = new Pathway();
-//        databaseObject.setDbId(123l);
-//        time = System.currentTimeMillis() - start;
-
-//        writer.write(time.toString() + "\n");
-//        writer.flush();
-        return databaseObject;
+    public String getAllDiseases() {
+        return StringUtils.join(generalService.findSimpleReferencesByClassName(Disease.class.getSimpleName()), "\n");
     }
 
-    @ApiOperation(value = "Retrieves a list of participants for a given pathway",response = DatabaseObject.class, responseContainer = "List", produces = "application/json")
-    @RequestMapping(value = "/detail/{id}/participatingPhysicalEntities", method = RequestMethod.GET, produces = "application/json")
+    @ApiOperation(value = "Retrieves all reference Molecules",response = String.class, produces = "text/plain")
+    @RequestMapping(value = "/getReferenceMolecules", method = RequestMethod.GET, produces = "text/plain")
     @ResponseBody
-    public Collection<PhysicalEntity> getParticipatingPhysicalEntities(@ApiParam(defaultValue = "R-HSA-5205685",required = true) @PathVariable String id)  {
-        return participantService.getParticipatingPhysicalEntities(id);
+    public String getAllReferenceMolecules() {
+        return StringUtils.join(generalService.findSimpleReferencesByClassName(ReferenceMolecule.class.getSimpleName()), "\n");
     }
 
-    @ApiOperation(value = "Retrieves a list of participants for a given pathway",response = Participant.class, responseContainer = "List", produces = "application/json")
-    @RequestMapping(value = "/detail/{id}/participants", method = RequestMethod.GET, produces = "application/json")
+    @ApiOperation(value = "Retrieves all Uniprot Reference Sequences",response = String.class, produces = "text/plain")
+    @RequestMapping(value = "/getUniProtRefSeqs", method = RequestMethod.GET, produces = "text/plain")
     @ResponseBody
-    public Collection<Participant> getParticipatingPhysicalEntities2(@ApiParam(defaultValue = "5205685",required = true) @PathVariable Long id)  {
-        return participantService.getParticipants(id);
+    public String getAllUniProtRefSeqs() {
+        return StringUtils.join(generalService.findSimpleReferencesByClassName(ReferenceGeneProduct.class.getSimpleName()), "\n");
     }
 
-    @ApiOperation(value = "Retrieves a list of participants for a given pathway",response = DatabaseObject.class, responseContainer = "List", produces = "application/json")
-    @RequestMapping(value = "/detail/{id}/participatingReferenceEntities", method = RequestMethod.GET, produces = "application/json")
+    @ApiOperation(value = "Retrieves all Persons containing name",response = Person.class, responseContainer = "List", produces = "application/json")
+    @RequestMapping(value = "/queryPeopleByName/{name}", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public Collection<ReferenceEntity> getParticipationgReferenceEntities(@ApiParam(defaultValue = "5205685",required = true) @PathVariable Long id)  {
-        return participantService.getParticipatingReferenceEntities(id);
+    public Collection<Person> queryPersonByName(@ApiParam(defaultValue = "Steve",required = true) @PathVariable String name) {
+        return generalService.queryPersonByName(name);
+    }
+
+    @ApiOperation(value = "Retrieves all Persons with name",response = Person.class, responseContainer = "List", produces = "application/json")
+    @RequestMapping(value = "/findPersonByName/{name}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Collection<Person> findPersonByName(@ApiParam(defaultValue = "Steve",required = true) @PathVariable String name) {
+        return generalService.findPersonByName(name);
+    }
+
+    @ApiOperation(value = "Retrieves all Persons with name",response = Person.class, responseContainer = "List", produces = "application/json")
+    @RequestMapping(value = "/queryPersonByEmail/{email:.+}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public Collection<Person> findPersonByEmail(@PathVariable String email) {
+        return generalService.findAllByProperty(Person.class,"eMailAddress", email, 0);
     }
 
     @ApiOperation(value = "Retrieves a list of databaseObjects for given class name",response = DatabaseObject.class, responseContainer = "List", produces = "application/json")
     @RequestMapping(value = "/details/{className:.*}", method = RequestMethod.GET)
     @ResponseBody
     public Collection<DatabaseObject> getDatabaseObjectsForClassName(@ApiParam(defaultValue = "Species",required = true) @PathVariable String className,
-                                                               @ApiParam(defaultValue = "1", required = true) @RequestParam(required = true) Integer page,
-                                                               @ApiParam(defaultValue = "25", required = true) @RequestParam(required = true) Integer offset) throws ClassNotFoundException {
+                                                                     @ApiParam(defaultValue = "1", required = true) @RequestParam(required = true) Integer page,
+                                                                     @ApiParam(defaultValue = "25", required = true) @RequestParam(required = true) Integer offset) throws ClassNotFoundException {
         return generalService.findObjectsByClassName(className,page,offset);
+    }
+
+    @ApiIgnore
+    @RequestMapping(value = "/details/legacy/{className:.*}", method = RequestMethod.GET)
+    @ResponseBody
+    public Collection<DatabaseObject> getDatabaseObjectsForClassNameLegacy(@PathVariable String className) throws ClassNotFoundException {
+        return generalService.findObjectsByClassName(className);
+    }
+
+
+    @ApiOperation(value = "Retrieves details of a databaseObject",response = DatabaseObject.class, produces = "application/json")
+    @RequestMapping(value = "/detail2/{id}", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public DatabaseObject findByIdWithOutgoingRelations(@ApiParam(defaultValue = "R-HSA-1640170",required = true) @PathVariable String id) throws IOException {
+        DatabaseObject databaseObject = generalService.find(id, RelationshipDirection.OUTGOING);
+        return databaseObject;
     }
 
     @ApiOperation(value = "Retrieves the list of the lower level pathways where the passed PhysicalEntity or Event are present", response = SimpleDatabaseObject.class, responseContainer = "List", produces = "application/json")
@@ -120,26 +129,5 @@ public class GraphController {
         Collection<SimpleDatabaseObject> rtn = generalService.getPathwaysFor(stableIdentifier, speciesId);
         return rtn;
     }
-
-    @ApiIgnore
-    @RequestMapping(value = "/details/legacy/{className:.*}", method = RequestMethod.GET)
-    @ResponseBody
-    public Collection<DatabaseObject> getDatabaseObjectsForClassNameLegacy(@PathVariable String className) throws ClassNotFoundException {
-        return generalService.findObjectsByClassName(className,1, 2000000); //Integer.MAX_VALUE is too big for neo4j
-    }
-
-//    @ApiOperation(value = "Retrieves details of a databaseObject",response = DatabaseObject.class, produces = "application/json")
-//    @RequestMapping(value = "/detail/{id}", method = RequestMethod.GET, produces = "application/json")
-//    @ResponseBody
-//    public Event queryById(@ApiParam(defaultValue = "R-HSA-1640170",required = true) @PathVariable String id)  {
-//        return eventService.findByIdWithLegacyFields(id);
-//    }
-//
-//    @ApiOperation(value = "Retrieves details of a databaseObject",response = DatabaseObject.class, produces = "application/json")
-//    @RequestMapping(value = "/detail/{id}", method = RequestMethod.GET, produces = "application/json")
-//    @ResponseBody
-//    public PhysicalEntity queryById(@ApiParam(defaultValue = "R-HSA-1640170",required = true) @PathVariable String id)  {
-//        return physicalEntityService.findByIdWithLegacyFields(id);
-//    }
 
 }
