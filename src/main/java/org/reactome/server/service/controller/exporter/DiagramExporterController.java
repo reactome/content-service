5bf5d61731e95b0d0e1c4b8f4ae8d3391a6cb30b
gviteri <gviteri@ebi.ac.uk>
2017-02-13 15:59:27 +0000

    Handling temporary file and decorators. Changed the way the file is returned. Not in Spring Context anymore

--- src/main/java/org/reactome/server/service/controller/exporter/DiagramExporterController.java
+++ src/main/java/org/reactome/server/service/controller/exporter/DiagramExporterController.java
@@ -1,24 +1,29 @@
 package org.reactome.server.service.controller.exporter;
 
 import io.swagger.annotations.*;
+import org.apache.commons.io.IOUtils;
 import org.reactome.server.service.manager.DiagramExportManager;
 import org.reactome.server.tools.diagram.exporter.common.profiles.factory.DiagramJsonDeserializationException;
 import org.reactome.server.tools.diagram.exporter.common.profiles.factory.DiagramJsonNotFoundException;
 import org.reactome.server.tools.diagram.exporter.common.profiles.factory.DiagramProfileException;
-import org.reactome.server.tools.diagram.exporter.pptx.exception.LicenseException;
+import org.reactome.server.tools.diagram.exporter.pptx.model.Decorator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
 
 /**
  * @author Guilherme S Viteri <gviteri@ebi.ac.uk>
  */
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "ConstantConditions"})
 @RestController
 @Api(tags = "exporter", description = "Diagram Exporters")
 @RequestMapping("/exporter")
@@ -39,15 +44,38 @@ public class DiagramExporterController {
             @ApiResponse(code = 503, message = "Service was unable to export to Power Point.")
     })
     @RequestMapping(value = "/diagram/{stId}" + PPT_FILE_EXTENSION, method = RequestMethod.GET)
-    @ResponseBody
-    public FileSystemResource toPPTX(@ApiParam(value = "Stable Identifier", required = true, defaultValue = "R-HSA-177929")
+    public void toPPTX(@ApiParam(value = "Stable Identifier", required = true, defaultValue = "R-HSA-177929")
                                      @PathVariable String stId,
                                      @ApiParam(value = "Diagram Color Profile", defaultValue = "Modern", allowableValues = "Modern, Standard")
                                      @RequestParam(value = "profile", defaultValue = "Modern", required = false) String colorProfile,
-                                     HttpServletResponse response) throws DiagramJsonNotFoundException, DiagramJsonDeserializationException, DiagramProfileException, LicenseException {
+                                     @ApiParam(value = "Flag element(s) in the diagram. CSV line.")
+                                     @RequestParam(value = "flg", required = false) List<Long> flg,
+                                     @ApiParam(value = "Highlight element(s) selection in the diagram. CSV line.")
+                                     @RequestParam(value = "sel", required = false) List<Long> sel,
+                                     HttpServletResponse response) throws DiagramJsonNotFoundException, DiagramJsonDeserializationException, DiagramProfileException, IOException {
+
         // IMPORTANT: Downloading the file on Swagger does not work - https://github.com/swagger-api/swagger-ui/issues/2132
+        // for this reason we are keeping this method as APIIgnore
         infoLogger.info("Exporting the Diagram {} to PPTX for color profile {}", stId, colorProfile);
 
-        return new FileSystemResource(manager.toPPTX(stId, colorProfile, response));
+        Decorator decorator = new Decorator(flg, sel);
+        File pptx =  manager.getPPTX(stId, colorProfile, decorator, response);
+
+        // when returning a FileSystemResource using Spring, then the file won't be deleted because it still has the
+        // reference to the file and then we cannot delete. Writing the file directly in the response allows us to
+        // delete only the temporary file.
+        OutputStream out = response.getOutputStream();
+        FileInputStream in = new FileInputStream(pptx);
+        IOUtils.copy(in,out);
+        out.flush();
+        out.close();
+        in.close();
+
+        // deleting the file in case it is decorated.
+        if(decorator.isDecorated()) {
+            if(!pptx.delete()) {
+                infoLogger.error("Could not delete the temporary file {}", pptx.getPath());
+            }
+        }
     }
 }
