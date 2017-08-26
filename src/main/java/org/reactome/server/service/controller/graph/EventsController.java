e8736f4e7138c71760ce0539dfa529ecaf232df4
Antonio Fabregat <fabregat.antonio@gmail.com>
2017-08-26 13:55:46 +0100

    participant methods removed

--- src/main/java/org/reactome/server/service/controller/graph/EventsController.java
+++ src/main/java/org/reactome/server/service/controller/graph/EventsController.java
@@ -2,12 +2,8 @@ package org.reactome.server.service.controller.graph;
 
 import io.swagger.annotations.*;
 import org.reactome.server.graph.domain.model.Pathway;
-import org.reactome.server.graph.domain.model.PhysicalEntity;
-import org.reactome.server.graph.domain.model.ReferenceEntity;
-import org.reactome.server.graph.domain.result.Participant;
 import org.reactome.server.graph.service.EventsService;
 import org.reactome.server.graph.service.HierarchyService;
-import org.reactome.server.graph.service.ParticipantService;
 import org.reactome.server.graph.service.helper.PathwayBrowserNode;
 import org.reactome.server.service.exception.ErrorInfo;
 import org.reactome.server.service.exception.NotFoundException;
@@ -30,14 +26,14 @@ public class EventsController {
 
     private static final Logger infoLogger = LoggerFactory.getLogger("infoLogger");
 
-    @Autowired
-    private HierarchyService eventHierarchyService;
+    private final HierarchyService eventHierarchyService;
+    private final EventsService eventsService;
 
     @Autowired
-    private ParticipantService participantService;
-
-    @Autowired
-    private EventsService eventsService;
+    public EventsController(HierarchyService eventHierarchyService, EventsService eventsService) {
+        this.eventHierarchyService = eventHierarchyService;
+        this.eventsService = eventsService;
+    }
 
     @ApiOperation(value = "The ancestors of a given event", notes = "The Reactome definition of events includes pathways and reactions. Although events are organised in a hierarchical structure, a single event can be in more than one location, i.e. a reaction can take part in different pathways while, in the same way, a sub-pathway can take part in many pathways. Therefore, this method retrieves a list of all possible paths from the requested event to the top level pathway(s).")
     @ApiResponses({
@@ -68,53 +64,6 @@ public class EventsController {
         if (pathwayBrowserNodes == null || pathwayBrowserNodes.isEmpty()) throw new NotFoundException("No event hierarchy found for given species: " + species);
         infoLogger.info("Request for full event hierarchy");
         return pathwayBrowserNodes;
-    }
-
-    @ApiOperation(value = "A list of participants for a given event",
-            notes = "Participants contains a PhysicalEntity (dbId, displayName) and a collection of ReferenceEntities (dbId, name, identifier, url)",
-            response = Participant.class, responseContainer = "List")
-    @ApiResponses({
-            @ApiResponse(code = 404, message = "Identifier does not match with any in current data", response = ErrorInfo.class),
-            @ApiResponse(code = 406, message = "Not acceptable according to the accept headers sent in the request", response = ErrorInfo.class),
-            @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorInfo.class)
-    })
-    @RequestMapping(value = "/event/{id}/participants", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    public Collection<Participant> getParticipants(@ApiParam(value = "DbId or StId of a PhysicalEntity", defaultValue = "5205685",required = true) @PathVariable String id)  {
-        Collection<Participant> participants = participantService.getParticipants(id);
-        if (participants == null || participants.isEmpty())  throw new NotFoundException("No participants found for id: " + id);
-        infoLogger.info("Request for participants of event with id: {}", id);
-        return participants;
-    }
-
-    @ApiOperation(value = "A list of participating PhysicalEntities for a given event", notes = "This method retrieves all the PhysicalEntities that take part in a given event. It is worth mentioning that because a pathway can contain smaller pathways (subpathways), the method also recursively retrieves the PhysicalEntities from every constituent pathway.")
-    @ApiResponses({
-            @ApiResponse(code = 404, message = "Identifier does not match with any in current data", response = ErrorInfo.class),
-            @ApiResponse(code = 406, message = "Not acceptable according to the accept headers sent in the request", response = ErrorInfo.class),
-            @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorInfo.class)
-    })
-    @RequestMapping(value = "/event/{id}/participatingPhysicalEntities", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    public Collection<PhysicalEntity> getParticipatingPhysicalEntities(@ApiParam(value = "The event for which the participating PhysicalEntities are requested", defaultValue = "R-HSA-5205685",required = true) @PathVariable String id)  {
-        Collection<PhysicalEntity> participants = participantService.getParticipatingPhysicalEntities(id);
-        if (participants == null || participants.isEmpty())  throw new NotFoundException("No participants found for id: " + id);
-        infoLogger.info("Request for participants of event with id: {}", id);
-        return participants;
-    }
-
-    @ApiOperation(value = "A list of participating ReferenceEntities for a given event", notes = "PhysicalEntity instances that represent, e.g., the same chemical in different compartments, or different post-translationally modified forms of a single protein, share numerous invariant features such as names, molecular structure and links to external databases like UniProt or ChEBI.<br>To enable storage of this shared information in a single place, and to create an explicit link among all the variant forms of what can also be seen as a single chemical entity, Reactome creates instances of the separate ReferenceEntity class. A ReferenceEntity instance captures the invariant features of a molecule.<br>This method retrieves the ReferenceEntities of all PhysicalEntities that take part in a given event. It is worth mentioning that because a pathway can contain smaller pathways (subpathways), this method also recursively retrieves the ReferenceEntities for all PhysicalEntities in every constituent pathway.")
-    @ApiResponses({
-            @ApiResponse(code = 404, message = "Identifier does not match with any in current data", response = ErrorInfo.class),
-            @ApiResponse(code = 406, message = "Not acceptable according to the accept headers sent in the request", response = ErrorInfo.class),
-            @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorInfo.class)
-    })
-    @RequestMapping(value = "/event/{id}/participatingReferenceEntities", method = RequestMethod.GET, produces = "application/json")
-    @ResponseBody
-    public Collection<ReferenceEntity> getParticipatingReferenceEntities(@ApiParam(value = "The event for which the participating ReferenceEntities are requested", defaultValue = "5205685",required = true) @PathVariable String id)  {
-        Collection<ReferenceEntity> participants = participantService.getParticipatingReferenceEntities(id);
-        if (participants == null || participants.isEmpty())  throw new NotFoundException("No participants found for id: " + id);
-        infoLogger.info("Request for participants of event with id: {}", id);
-        return participants;
     }
 
 }
