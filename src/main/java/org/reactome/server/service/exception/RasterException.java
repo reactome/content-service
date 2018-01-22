dcec62710b3f27e05cfe8a2102a003f03540371d
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-01-22 17:11:50 +0000

    raster generic exception

---
+++ src/main/java/org/reactome/server/service/exception/RasterException.java
@@ -0,0 +1,9 @@
+package org.reactome.server.service.exception;
+
+public class RasterException extends RuntimeException {
+
+    public RasterException(String message) {
+        super(message);
+    }
+
+}
