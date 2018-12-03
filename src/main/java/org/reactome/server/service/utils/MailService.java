cdbc23398011c114ec481ea3bdce2fa3ae3fb149
Antonio Fabregat <fabregat.antonio@gmail.com>
2018-12-03 11:12:39 +0000

    changes in the notification messages

--- src/main/java/org/reactome/server/service/utils/MailService.java
+++ src/main/java/org/reactome/server/service/utils/MailService.java
@@ -3,7 +3,6 @@ package org.reactome.server.service.utils;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import javax.mail.Message;
@@ -19,24 +18,16 @@ public class MailService {
         this.mailSender = mailSender;
     }
 
-    public void send(final String fromName, final String fromAddress, final String toAddress, final String subject, final String msgBody) {
-        this.send(fromName, fromAddress, toAddress, subject, msgBody, false);
-    }
-
-    public void send(final String fromName, final String fromAddress, final String toAddress, final String subject, final String msgBody, final Boolean sendEmailCopy) {
-        MimeMessagePreparator preparator = mimeMessage -> {
+    public void start(final String fromName, final String fromAddress, final String toAddress, final String subject, final String msgBody) {
+        mailSender.send(mimeMessage -> {
             mimeMessage.setRecipient(Message.RecipientType.TO, new InternetAddress(toAddress));
             InternetAddress from = new InternetAddress(fromAddress);
             if (StringUtils.isNotBlank(fromName)) {
                 from = new InternetAddress(fromAddress, fromName);
             }
-            if (sendEmailCopy) {
-                mimeMessage.setRecipient(Message.RecipientType.BCC, new InternetAddress(fromAddress));
-            }
             mimeMessage.setFrom(from);
             mimeMessage.setSubject(subject);
             mimeMessage.setText(msgBody);
-        };
-        mailSender.send(preparator);
+        });
     }
 }
