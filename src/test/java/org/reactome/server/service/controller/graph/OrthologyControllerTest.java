1458762ef0a854cbaa8458ccc7d40103ba8fd1f6
cgong <cgong@ebi.ac.uk>
2019-12-16 09:30:30 +0000

    create orthology and person controller

--- src/test/java/org/reactome/server/service/controller/graph/OrthologyControllerTest.java
+++ src/test/java/org/reactome/server/service/controller/graph/OrthologyControllerTest.java
@@ -9,33 +9,21 @@ import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.junit.Assert.*;
 
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"file:src/test/resources-test/mvc-dispatcher-servlet-test.xml"})
 @WebAppConfiguration
-public class PersonControllerTest extends BaseTest {
+public class OrthologyControllerTest extends BaseTest {
 
     @Test
-    public void findPersonByName() throws Exception {
-        mvcGetResult("/data/people/name/Bijay Jassal","application/json;Charset=UTF-8");
+    public void getOrthology() throws Exception {
+        mvcGetResult("/data/orthology/8956320/species/48898", "application/json;Charset=UTF-8");
     }
 
+    //todo post parameter
     @Test
-    public void queryPersonByName() {
-    }
+    public void getOrthologies() throws Exception {
 
-    @Test
-    public void findPerson() {
-    }
-
-    @Test
-    public void testFindPerson() {
-    }
-
-    @Test
-    public void getPublicationsOfPerson() {
-    }
-
-    @Test
-    public void getAuthoredPathways() {
+        mvcPostResult("/data/orthologies/ids/species/49633", "R-HSA-6799198,R-HSA-446203, R-HSA-4086398");
     }
 }
