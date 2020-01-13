7197d01a12bafeece7a1b92e108ab02cf4b5fc0c
cgong <cgong@ebi.ac.uk>
2020-01-13 17:00:00 +0000

    get token from filepath

--- src/test/java/org/reactome/server/service/controller/interactors/TokenControllerTest.java
+++ src/test/java/org/reactome/server/service/controller/interactors/TokenControllerTest.java
@@ -1,11 +1,18 @@
 package org.reactome.server.service.controller.interactors;
 
+import org.apache.commons.io.comparator.LastModifiedFileComparator;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.reactome.server.service.utils.BaseTest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -14,10 +21,33 @@ import static org.junit.Assert.*;
 @WebAppConfiguration
 public class TokenControllerTest extends BaseTest {
 
+    @Value("${tuples.uploaded.files.folder:/Users/reactome/Reactome/custom}")
+    private String tokenFolder;
+
     @Test
     public void getInteractors() throws Exception {
 
+        String token = getLastestPSIFile(tokenFolder);
+
+        /*
+          To test it, you will need a valid token, which can be generated when running the CustomPsicquicControllerTest.
+          The token (PSI--XXXXX.bin) will be written in the file system based on the <tuples.uploaded.files.folder> property.
+          e.g <tuples.uploaded.files.folder>/Users/reactome/Reactome/custom</tuples.uploaded.files.folder>
+         */
+
         //Todo this token only works for local testing
-        mvcPostResult("/interactors/token/PSI--1403260106", "Q9UBU9");
+        mvcPostResult("/interactors/token/" + token, "Q9UBU9");
+    }
+
+    /* Get the newest file for a specific extension */
+    private String getLastestPSIFile(String filePath) {
+        File dir = new File(filePath);
+        FileFilter fileFilter = new WildcardFileFilter("PSI*");
+        File[] files = dir.listFiles(fileFilter);
+        if (files != null && files.length > 0) {
+            Arrays.sort(files, LastModifiedFileComparator.LASTMODIFIED_REVERSE);
+            return files[0].getName();
+        }
+        return "-";
     }
 }
