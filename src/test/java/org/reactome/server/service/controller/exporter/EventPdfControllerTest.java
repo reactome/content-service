c952e786285dff52724fb2d65f62e2ba87cf2b45
cgong <cgong@ebi.ac.uk>
2020-01-16 10:54:58 +0000

    create PDFExporter controller

---
+++ src/test/java/org/reactome/server/service/controller/exporter/EventPdfControllerTest.java
@@ -0,0 +1,29 @@
+package org.reactome.server.service.controller.exporter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.reactome.server.service.utils.BaseTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"file:src/test/resources-test/mvc-dispatcher-servlet-test.xml"})
+@WebAppConfiguration
+public class EventPdfControllerTest extends BaseTest {
+
+    @Test
+    public void eventPdf() throws Exception {
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("level", 1);
+        params.put("diagramProfile", "Modern");
+        params.put("resource", "total");
+        params.put("expColumn", 1);
+        params.put("analysisProfile", "Standard");
+        mockMvcGetResult("/exporter/document/event/R-HSA-1632852.pdf", "application/pdf", params);
+    }
+}
