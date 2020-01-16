47fb54af7f7e53837322b82fa9623d6e2821699f
cgong <cgong@ebi.ac.uk>
2020-01-16 12:18:10 +0000

    typo

--- src/test/java/org/reactome/server/service/controller/graph/SchemaControllerTest.java
+++ src/test/java/org/reactome/server/service/controller/graph/SchemaControllerTest.java
@@ -10,8 +10,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.junit.Assert.*;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"file:src/test/resources-test/mvc-dispatcher-servlet-test.xml"})
 @WebAppConfiguration
@@ -20,33 +18,33 @@ public class SchemaControllerTest extends BaseTest {
     @Test
     public void getDatabaseObjectsForClassName() throws Exception {
 
-        Map<String, Object> parmas = new HashMap<>();
-        parmas.put("species", 9606);
-        parmas.put("page", 1);
-        parmas.put("offset", 20);
+        Map<String, Object> params = new HashMap<>();
+        params.put("species", 9606);
+        params.put("page", 1);
+        params.put("offset", 20);
 
-        mockMvcGetResult("/data/schema/TopLevelPathway", "Application/json;Charset=UTF-8", parmas);
+        mockMvcGetResult("/data/schema/TopLevelPathway", "Application/json;Charset=UTF-8", params);
     }
 
     @Test
     public void getSimpleDatabaseObjectByClassName() throws Exception {
 
-        Map<String, Object> parmas = new HashMap<>();
-        parmas.put("species", 9606);
-        parmas.put("page", 1);
-        parmas.put("offset", 20);
+        Map<String, Object> params = new HashMap<>();
+        params.put("species", 9606);
+        params.put("page", 1);
+        params.put("offset", 20);
 
-        mockMvcGetResult("/data/schema/TopLevelPathway/min", "Application/json;Charset=UTF-8", parmas);
+        mockMvcGetResult("/data/schema/TopLevelPathway/min", "Application/json;Charset=UTF-8", params);
     }
 
     @Test
     public void getSimpleReferencesObjectsByClassName() throws Exception {
 
-        Map<String, Object> parmas = new HashMap<>();
-        parmas.put("page", 1);
-        parmas.put("offset", 200);
+        Map<String, Object> params = new HashMap<>();
+        params.put("page", 1);
+        params.put("offset", 200);
 
-        mockMvcGetResult("/data/schema/ReferenceGroup/reference", "Application/json;Charset=UTF-8", parmas);
+        mockMvcGetResult("/data/schema/ReferenceGroup/reference", "Application/json;Charset=UTF-8", params);
     }
 
     @Test
