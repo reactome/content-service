f99f2b743c88815783b41bc44f14127f59314c31
Guilherme Viteri <gviteri@ebi.ac.uk>
2020-02-26 14:57:16 +0000

    produces json

--- src/main/java/org/reactome/server/service/controller/citation/CitationController.java
+++ src/main/java/org/reactome/server/service/controller/citation/CitationController.java
@@ -9,6 +9,7 @@ import org.reactome.server.graph.service.AdvancedDatabaseObjectService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,7 +37,7 @@ public class CitationController {
     private AdvancedDatabaseObjectService advancedDatabaseObjectService;
 
     @ApiIgnore
-    @GetMapping(value = "/pathway/{id}")
+    @GetMapping(value = "/pathway/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> pathwayCitation(@ApiParam(value = "DbId or StId of the requested database object", required = true)
                                              @PathVariable String id) {
 
